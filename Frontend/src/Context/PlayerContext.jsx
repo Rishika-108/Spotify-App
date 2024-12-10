cf2f846f2f9da3e7134ba62347296114974f7dd9
Rishika Thakur <159441865+Rishika-108@users.noreply.github.com>
2024-12-10 21:07:20 +0530

    Update PlayerContext.jsx

--- Frontend/src/Context/PlayerContext.jsx
+++ Frontend/src/Context/PlayerContext.jsx
@@ -9,7 +9,7 @@ const audioRef = useRef();
 const seekBg = useRef ();
 const seekBar = useRef ();
 
-const url = 'http://localhost:4000'
+const url = 'https://spotify-app-1-31xx.onrender.com'
 const [songsData, setSongsData] = useState ([]);
 const [albumsData, setAlbumsData]= useState([]);
 
